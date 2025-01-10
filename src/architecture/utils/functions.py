39627c9274afa7831bb5b2694bf1fbd9a2142ca1
Arthur Brenno <64020210+arthurbrenno@users.noreply.github.com>
2025-01-10 11:29:13 -0300

    wip

--- src/architecture/utils/functions.py
+++ src/architecture/utils/functions.py
@@ -23,14 +23,16 @@ def _file_get_contents_cached(filename: str) -> str:
     return Path(filename).read_text()
 
 
-def run_sync(
-    coro_func: Callable[P, Coroutine[Any, Any, T]], *args: P.args, **kwargs: P.kwargs
-) -> T:
+def run_sync(coro_func, *args, **kwargs):
+    """
+    Run an async coroutine in a synchronous context.
+    """
     try:
-        # Attempt to get a running loop in *this* thread
+        # If this succeeds, it means there's already a running loop in THIS thread
         loop = asyncio.get_running_loop()
-        # If we get here, a loop is running in this thread
-        return loop.run_until_complete(coro_func(*args, **kwargs))
     except RuntimeError:
-        # Means no loop is running in this thread
+        # No running loop -> safe to do `asyncio.run()`
         return asyncio.run(coro_func(*args, **kwargs))
+    else:
+        # There's a running loop in the current thread -> use that loop
+        return loop.run_until_complete(coro_func(*args, **kwargs))
